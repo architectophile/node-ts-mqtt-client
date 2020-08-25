ed4d788c8b9eb643ba70452fc3edf63c623b76ae
Byeongcheol Yoo <architectophile@gmail.com>
2020-08-25 16:34:22 +0900

    server topics added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import mqtt from "mqtt";
+import mqtt, { Packet } from "mqtt";
 import {
-  SEC_TOPIC_OPEN,
-  SEC_TOPIC_START_TLS,
-  SEC_TOPIC_REQ_ENC,
-  SEC_TOPIC_RES_ENC,
+  TOPIC_SEC_CLIENT_OPEN,
+  TOPIC_SEC_CLIENT_START_TLS,
+  TOPIC_SEC_CLIENT_REQ_ENC,
+  TOPIC_SEC_CLIENT_RES_ENC,
+  TOPIC_SEC_SERVER_RES_ENC,
 } from "./topics";
 
 const isSecure = false;
@@ -12,6 +13,8 @@ const port = isSecure ? "8883" : "1883";
 const protocol = isSecure ? "mqtts" : "mqtt";
 const protocolId = "MQTT";
 const protocolVersion = 4;
+
+const interval = 5000;
 
 const connectOptions = {
   host,
@@ -31,24 +34,44 @@ const connectOptions = {
 const mqtt_client = mqtt.connect(connectOptions);
 
 mqtt_client.on("connect", () => {
-  mqtt_client.subscribe(SEC_TOPIC_OPEN);
-  console.log("subscribe security topic open as " + SEC_TOPIC_OPEN);
+  mqtt_client.subscribe(TOPIC_SEC_CLIENT_OPEN);
+  console.log("subscribe security topic open as " + TOPIC_SEC_CLIENT_OPEN);
 
-  mqtt_client.subscribe(SEC_TOPIC_START_TLS);
-  console.log("subscribe security topic start TLS as " + SEC_TOPIC_START_TLS);
-
-  mqtt_client.subscribe(SEC_TOPIC_REQ_ENC);
+  mqtt_client.subscribe(TOPIC_SEC_CLIENT_START_TLS);
   console.log(
-    "subscribe security topic encryption request as " + SEC_TOPIC_REQ_ENC,
+    "subscribe security topic start TLS as " + TOPIC_SEC_CLIENT_START_TLS,
+  );
+
+  mqtt_client.subscribe(TOPIC_SEC_CLIENT_REQ_ENC);
+  console.log(
+    "subscribe security topic encryption request as " +
+      TOPIC_SEC_CLIENT_REQ_ENC,
+  );
+
+  mqtt_client.subscribe(TOPIC_SEC_SERVER_RES_ENC);
+  console.log(
+    "subscribe security topic encryption request as " +
+      TOPIC_SEC_SERVER_RES_ENC,
   );
 });
 
-mqtt_client.on("message", (topic: string, message: string) => {
-  console.log("message called.");
+mqtt_client.on("message", (topic: string, message: Buffer, packet: Packet) => {
+  console.log("client message called.");
   console.log("topic: ", topic);
-  console.log("message: ", message);
-  mqtt_client.publish(
-    SEC_TOPIC_RES_ENC,
-    `request topic: ${topic}, message: ${message}`,
-  );
+  console.log("message: ", message.toString());
+  console.log("packet: ", packet);
+  setTimeout(() => {
+    mqtt_client.publish(
+      TOPIC_SEC_CLIENT_RES_ENC,
+      `client eco > request topic: ${topic}`,
+    );
+  }, interval);
 });
+
+setTimeout(() => {
+  console.log("on client publish called.");
+  mqtt_client.publish(
+    TOPIC_SEC_CLIENT_RES_ENC,
+    "This is the message from the client.",
+  );
+}, interval * 2);
--- src/topics.ts
+++ src/topics.ts
@@ -1,5 +1,9 @@
-export const SEC_TOPIC_OPEN = "/nCube/security/open";
-export const SEC_TOPIC_START_TLS = "/nCube/security/open";
-export const SEC_TOPIC_REQ_ENC = "/nCube/security/req-enc";
+export const TOPIC_SEC_CLIENT_OPEN = "/nCube/security/client/open";
+export const TOPIC_SEC_CLIENT_START_TLS = "/nCube/security/client/start-tls";
+export const TOPIC_SEC_CLIENT_REQ_ENC = "/nCube/security/client/req-enc";
+export const TOPIC_SEC_CLIENT_RES_ENC = "/nCube/security/client/res-enc";
 
-export const SEC_TOPIC_RES_ENC = "/nCube/security/res-enc";
+export const TOPIC_SEC_SERVER_OPEN = "/nCube/security/server/open";
+export const TOPIC_SEC_SERVER_START_TLS = "/nCube/security/server/start-tls";
+export const TOPIC_SEC_SERVER_REQ_ENC = "/nCube/security/server/req-enc";
+export const TOPIC_SEC_SERVER_RES_ENC = "/nCube/security/server/res-enc";
